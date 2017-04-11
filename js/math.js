ecab462f84de8dc59bb8b5319415dba0721eda29
raisking <raiprakas007@gmail.com>
2017-04-11 10:36:30 -0400

    updated

---
+++ js/math.js
@@ -0,0 +1,4 @@
+var name =prompt('What is your name?');
+var last =prompt('What is your lastname?');
+var fullname = "<h2>His name is" + name + "<h2> and his last name is " + last;
+document.write(fullname);
